fd5becdc2017b489fded87f43929aa3bef9732f4
Dinh Danh Le <dinh.danh.le@ericsson.com>
2025-08-13 13:17:36 +0100

    initial commit for our first Spring boot prject

---
+++ src/main/java/com/devops/FullstackwebApplication.java
@@ -0,0 +1,14 @@
+package com.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackwebApplication.class, args);
+		System.out.println("Hello STS World !");
+	}
+
+}
