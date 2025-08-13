c0e18572317d802ebc7498a244aafc05f52e72a5
Dinh Danh Le <dinh.danh.le@ericsson.com>
2025-08-13 13:34:49 +0100

    added HelloWorld Controller and index.html

---
+++ src/main/java/com/devops/controllers/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.devops.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+	@GetMapping("/")
+	public String sayHello() {
+		return "index";
+	}
+}
